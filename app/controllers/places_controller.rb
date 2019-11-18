fa0bbeb0eca5d462d539921d0e34d112ffdd3fda
“Zach <z_wood@baylor.edu>
2019-11-18 03:05:30 +0000

    added email tag to bottom of page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,8 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @places = Place.all 
+    @places = Place.all
   end
 
   def new
@@ -9,7 +10,7 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
@@ -18,5 +19,4 @@ class PlacesController < ApplicationController
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
-
 end
